fa15cbc14a728e1577379a444e0cb25ac40cddce
Hunter Lintz <hunterlintz126@gmail.com>
2020-04-27 19:21:00 -0400

    Added Tables into js file and added link to js file in html
    Added Tables into js file and added link to js file in html

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,17 @@
+var tableArray = [
+	{
+		customerName: 'Matt',
+		customerEmail: 'matt@gmail.com',
+		customerID: 'cust1',
+		PhoneNumber: '000-000-0000'
+	}
+];
+
+var waitingArray = [
+  	{
+	customerName: 'Ralph',
+	customerEmail: 'ralph@gmail.com',
+	customerID: 'cust2',
+	PhoneNumber: '000-000-0000'
+	}
+];
