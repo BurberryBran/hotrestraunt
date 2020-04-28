b63323e34f926d8d97286b3859f985684744f781
Hunter Lintz <hunterlintz126@gmail.com>
2020-04-27 20:09:42 -0400

    added some of the basic js
    added some of the basic js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,13 @@
-var tableArray = [
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var reserveArray = [
 	{
 		customerName: 'Matt',
 		customerEmail: 'matt@gmail.com',
@@ -15,3 +24,21 @@ var waitingArray = [
 	PhoneNumber: '000-000-0000'
 	}
 ];
+
+app.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname, "../../index.html"));
+});
+
+app.get("/tables", function(req, res) {
+	res.sendFile(path.join(__dirname, "../../tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+	res.sendFile(path.join(__dirname, "../../reserve.html"));
+});
+
+for(var i = 0; i < reserveArray.length; i++)
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT " + PORT);
+});
